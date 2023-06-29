53dc83aa3c3c336312a72e6ff9865a069ba048cb
Payton Pfannenstiel <payton02@ksu.edu>
2023-06-29 16:46:30 -0500

    Added basic climbing

--- Assets/_Scripts/Player/PlayerMovement.cs
+++ Assets/_Scripts/Player/PlayerMovement.cs
@@ -9,18 +9,23 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float speed = 1f;
     [SerializeField] float jumpHeight = 1f;
     [SerializeField] LayerMask ground;
+    [SerializeField] float climbSpeed = 1f;
+    [SerializeField] LayerMask climbing;
 
     Rigidbody2D playerRb;
     BoxCollider2D playerCollider;
     Vector2 moveInput;
+    float initialPlayerGravity;
 
     void Start() {
         playerRb = GetComponent<Rigidbody2D>();
         playerCollider = GetComponent<BoxCollider2D>();
+        initialPlayerGravity = playerRb.gravityScale;
     }
 
     void Update() {
         Run();
+        Climb();
         FlipSprite();
     }
 
@@ -41,6 +46,13 @@ public class PlayerMovement : MonoBehaviour
 
     private void Run() {
         playerRb.velocity = new Vector2(moveInput.x * speed, playerRb.velocity.y);
+    }
+
+    private void Climb() {
+        if (playerCollider.IsTouchingLayers(climbing)) {
+            playerRb.gravityScale = 0;
+            /* if (Math.Abs(moveInput.y) > Mathf.Epsilon)  */playerRb.velocity = new Vector2(playerRb.velocity.x, moveInput.y * climbSpeed);
+        } else playerRb.gravityScale = initialPlayerGravity;
     }
 
     void FlipSprite() {
